8811386b1ff9d648fe8d35c9ea8250414ee4e0ff
Joshua Torrance <joshua.torrance@anu.edu.au>
2025-11-11 16:07:57 +1100

    Add MOPPy mapping with some string parsing to table

--- scripts/schema2md.py
+++ scripts/schema2md.py
@@ -29,6 +29,7 @@ MAPPING_COLS = {
     "units": "Units",
     "frequency": "Frequency",
     "esm15_name": "ESM1.5 Name",
+    "esm15_mapping": "ESM1.5 Mapping",
     "cmip6_compound_name": "CMIP6 Compound Name",
     "cmip7_compound_name": "CMIP7 Compound Name",
 }
@@ -159,6 +160,35 @@ def schema2md(schema_url, dot_point_lists=True):
     return global_md_str, variable_md_str
 
 
+def _parse_mapping(map_d):
+    if isinstance(map_d, dict):
+        if 'type' in map_d and map_d['type'] == 'direct':
+            return ""
+        else:
+            op = map_d["operation"]
+            join_str = ", "
+            if op == "multiply":
+                join_str = " * "
+                op = ""
+            elif op == "add":
+                join_str = " + "
+                op = ""
+            elif op == "subtract":
+                join_str = " - "
+                op = ""
+        
+            try:
+                args = map_d["operands"]
+            except KeyError:
+                # Try 'args' instead of operands, sometimes these are numbers not strings
+                args = map_d["args"]
+            args = map(_parse_mapping, args)
+            args = join_str.join(args)
+            return f"{op}({args})"
+    else:
+        return str(map_d)
+
+
 def mapping2md():
     """
     Acquire mapping information from cached CMIP7 variable metadata and from
@@ -182,13 +212,25 @@ def mapping2md():
         moppy_var_keys = list(moppy_mapping.keys())
         assert len(moppy_var_keys) <= 1, "MOPPy unexpected returned more than one key for {cmip6_var} - {moppy_var_keys}."
 
-        # Add ACCESS variable to the dict
-        try:
-            esm_name = moppy_mapping[moppy_var_keys[0]]['model_variables']
-        except (KeyError, IndexError):
+        if moppy_mapping == {}:
             esm_name = 'unknown'
+            esm_mapping = 'unknown'
+        else:
+            # Add ACCESS variable to the dict
+            try:
+                esm_name = moppy_mapping[moppy_var_keys[0]]['model_variables']
+            except (KeyError, IndexError):
+                esm_name = 'unknown'
+
+            # Add the MOPPy mapping to the dict
+            esm_mapping = _parse_mapping(moppy_mapping[moppy_var_keys[0]]['calculation'])
+
+            # Remove parentheses from simple mappings (e.g. "(A+B)" -> "A+B")
+            if len(esm_mapping) > 2 and esm_mapping[0] == '(' and esm_mapping[-1] == ')':
+                esm_mapping = esm_mapping[1:-1]
 
         cmip7_core_vars[cmip7_var]['esm15_name'] = esm_name
+        cmip7_core_vars[cmip7_var]['esm15_mapping'] = esm_mapping
 
     # Convert CMIP7 dict into pandas df
     df = pd.DataFrame.from_records(cmip7_core_vars).T
