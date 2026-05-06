37adfea25ceaa92d9b301300b722c715e79a421f
joshuatorrance <joshuatorrance@gmail.com>
2026-05-06 17:00:48 +1000

    16 - New version of schema (2-1-0) (#19)
    * Updated to point to new version of schema
    
    * Commit from GitHub Actions (Build Docs)
    
    * Abstracted the proccing of subschema to md
    
    * Updated template and added table for time metadata
    
    * Commit from GitHub Actions (Build Docs)
    
    * Tweaked template and formatting for const entries
    
    * Commit from GitHub Actions (Build Docs)
    
    ---------
    
    Co-authored-by: joshuatorrance <joshuatorrance@users.noreply.github.com>

--- scripts/schema2md.py
+++ scripts/schema2md.py
@@ -14,6 +14,7 @@ from access_moppy.utilities import load_model_mappings
 GLOBAL_COLS = {
     "title": "Title",
     "description": "Description",
+    "type": "Type",
     "examples": "Examples",
     "rules": "Rules",
     "required": "Required",
@@ -23,6 +24,13 @@ VARIABLE_COLS = {
     "description": "Description",
     "type": "Type",
     "examples": "Examples",
+}
+TIME_COLS = {
+    "title": "Title",
+    "description": "Description",
+    "type": "Type",
+    "examples": "Examples",
+    "required": "Required",
 }
 
 MAPPING_COLS = {
@@ -34,6 +42,126 @@ MAPPING_COLS = {
     "esm15_name": "ACCESS Name",
     "esm15_mapping": "Mapping",
 }
+
+
+def process_subschema(subschema_dict, required, cols, dot_point_lists=True):
+    # Build pandas tables out of the json
+    df = pd.DataFrame.from_records(subschema_dict['properties']).T
+
+    # Add required
+    df = df.assign(required=["Yes" if row_name in required else "No" for row_name in df.index])
+
+    # Escape |s in regex patterns
+    try:
+        df["pattern"] = df["pattern"].str.replace("|", "\\|")
+    except KeyError:
+        # Not every schema has 'pattern'
+        pass
+
+    # oneOf is a list of dicts {pattern: regex}, convert to a list of strings
+    try:
+        df["oneOf"] = df["oneOf"].apply(
+            lambda x: [xi["pattern"] for xi in x] if isinstance(x, list) else x
+        )
+    except KeyError:
+        # Not every schema has 'oneOf'
+        pass
+
+    # Certain columns are lists, convert them to strings
+    for col_name in df.columns:
+        # dtype for list columns is object so we need to go grab one to check
+        col = df[col_name].dropna()
+        contents = col.iloc[0]
+
+        if isinstance(contents, list):
+            if dot_point_lists:
+                join_str = "</li><li>"
+                start_str = "<ul><li>"
+                end_str = "</li></ul>"
+            else:
+                join_str = ", "
+                start_str = ""
+                end_str = ""
+
+            def list2str(l):
+                if isinstance(l, list):
+                    if len(l) > 1:
+                        # Turn multi-item lists into dot point lists
+                        return (
+                            start_str
+                            + join_str.join([str(li) for li in l])
+                            + end_str
+                        )
+                    elif len(l) == 1:
+                        # Turn single items into just that item
+                        return l[0]
+                    else:
+                        # Empty string for empty lists
+                        return ""
+
+                # Otherwise just return the item as is
+                return l
+
+            df[col_name] = df[col_name].apply(list2str)
+
+    # Prefix these columns with some explanatory text first
+    # Then unify them into a new column "rules"
+    prefix_d = {
+        "pattern": "Must match regex: ",
+        "oneOf": "Must match one of these regex: ",
+        "enum": "Must be one of the following: ",
+    }
+    df["rules"] = pd.Series(index=df.index, name="pattern")
+    for key, prefix in prefix_d.items():
+        try:
+            df[key] = df[key].apply(
+                lambda x: prefix + x if isinstance(x, str) else x
+            )
+            df["rules"] = df["rules"].fillna(df[key])
+        except KeyError:
+            # Not every schema has 'pattern', 'oneOf', 'enum'
+            pass
+
+    # If "title" is missing fill it with the index
+    df["title"] = df["title"].fillna(df.index.to_series())
+
+    # If description is missing then use the const to give a description
+    try:
+        const_str = "Must have the value '{}'"
+        extended_const = df["const"].map(lambda s: const_str.format(s), na_action="ignore")
+        df["description"] = df["description"].fillna(extended_const)
+    except KeyError:
+        # Not every schema will have const
+        pass
+
+    # If example is missing, fill it with const if present
+    try:
+        df["examples"] = df["examples"].fillna(df["const"])
+    except KeyError:
+        # Not every schema will have const
+        pass
+
+    # If type is missing, fill it with the type of const if present
+    try:
+        # At the moment there are only two types
+        df["type"] = df["type"].fillna(df["const"].map(
+            lambda e: "string" if isinstance(e, str) else "number", na_action="ignore")
+        )
+    except KeyError:
+        # Not every schema will have const
+        pass
+
+    # Sort dataframe alphabetically by attribute names
+    df.sort_values("title", inplace=True, key=lambda col: col.str.lower())
+
+    # Replace nans with empty strings
+    df = df.fillna("")
+
+    # Filter and rename output columns
+    df = df[cols.keys()].rename(columns=cols)
+
+    return df.to_markdown(index=False, tablefmt="github")
+
 
 def schema2md(schema_url, dot_point_lists=True):
     """
@@ -51,114 +179,22 @@ def schema2md(schema_url, dot_point_lists=True):
     # Get the schema as a json
     schema = materialize(RefDict(schema_url))
 
-    # Get the global and variable attributes
-    global_attrs = schema["properties"]["global"]["properties"]
-    variable_attrs = schema["properties"]["variables"]["patternProperties"]["^.+$"][
-        "properties"
-    ]
+    # Process the global metadata    
+    global_attrs = schema["properties"]["global"]
+    global_required = global_attrs["required"]
+    global_md_str = process_subschema(global_attrs, global_required, GLOBAL_COLS)
 
-    # Build pandas tables out of the jsons
-    global_df = pd.DataFrame.from_records(global_attrs).T
-    variable_df = pd.DataFrame.from_records(variable_attrs).T
+    # Process the generic variable metadata
+    variable_attrs = schema["properties"]["variables"]
+    variable_required = global_attrs["required"]
+    variable_md_str = process_subschema(variable_attrs["patternProperties"]["^.+$"], variable_required, VARIABLE_COLS)
 
-    # Add required as a column to both dfs
-    def add_required(df, name):
-        if "required" in schema["properties"][name]:
-            required = schema["properties"][name]["required"]
-        else:
-            required = []
+    # Process metadata for the time variable
+    time_attrs =  variable_attrs["properties"]['time']
+    time_required = variable_attrs["properties"]['time']['required']
+    time_md_str = process_subschema(time_attrs, time_required, TIME_COLS)
 
-        df = df.assign(required=[row_name in required for row_name in df.index])
-
-        # Convert True/False to Yes/No
-        df["required"] = df["required"].replace({True: "Yes", False: "No"})
-
-        return df
-
-    global_df = add_required(global_df, "global")
-    variable_df = add_required(variable_df, "variables")
-
-    # Sort dataframe alphabetically by attribute names
-    global_df.sort_values("title", inplace=True, key=lambda col: col.str.lower())
-    variable_df.sort_values("title", inplace=True, key=lambda col: col.str.lower())
-
-    # Escape |s in regex patterns
-    global_df["pattern"] = global_df["pattern"].str.replace("|", "\\|")
-
-    # oneOf is a list of dicts {pattern: regex}, convert to a list of strings
-    global_df["oneOf"] = global_df["oneOf"].apply(
-        lambda x: [xi["pattern"] for xi in x] if isinstance(x, list) else x
-    )
-
-    # Certain columns are lists, convert them to strings
-    dot_point_lists = True
-    for df in [global_df, variable_df]:
-        for col_name in df.columns:
-            # dtype for list columns is object so we need to go grab one to check
-            col = df[col_name].dropna()
-            contents = col.iloc[0]
-
-            if isinstance(contents, list):
-                if dot_point_lists:
-                    join_str = "</li><li>"
-                    start_str = "<ul><li>"
-                    end_str = "</li></ul>"
-                else:
-                    join_str = ", "
-                    start_str = ""
-                    end_str = ""
-
-                def list2str(l):
-                    if isinstance(l, list):
-                        if len(l) > 1:
-                            # Turn multi-item lists into dot point lists
-                            return (
-                                start_str
-                                + join_str.join([str(li) for li in l])
-                                + end_str
-                            )
-                        elif len(l) == 1:
-                            # Turn single items into just that item
-                            return l[0]
-                        else:
-                            # Empty string for empty lists
-                            return ""
-
-                    # Otherwise just return the item as is
-                    return l
-
-                df[col_name] = df[col_name].apply(list2str)
-
-    # Prefix these columns with some explanatory text first
-    prefix_d = {
-        "pattern": "Must match regex: ",
-        "oneOf": "Must match one of these regex: ",
-        "enum": "Must be one of the following: ",
-    }
-
-    for key, prefix in prefix_d.items():
-        global_df[key] = global_df[key].apply(
-            lambda x: prefix + x if isinstance(x, str) else x
-        )
-
-    # Unify the pattern, oneOf, and enum columns
-    global_df["rules"] = (
-        global_df["pattern"].fillna(global_df["oneOf"]).fillna(global_df["enum"])
-    )
-
-    # Replace nans with empty strings
-    global_df = global_df.fillna("")
-    variable_df = variable_df.fillna("")
-
-    # Filter and rename output columns
-    global_final_df = global_df[GLOBAL_COLS.keys()].rename(columns=GLOBAL_COLS)
-    variable_final_df = variable_df[VARIABLE_COLS.keys()].rename(columns=VARIABLE_COLS)
-
-    # Format markdown table to files
-    global_md_str = global_final_df.to_markdown(index=False, tablefmt="github")
-    variable_md_str = variable_final_df.to_markdown(index=False, tablefmt="github")
-
-    return global_md_str, variable_md_str
+    return global_md_str, variable_md_str, time_md_str
 
 
 def _parse_mapping(map_d):
--- scripts/update_specification.py
+++ scripts/update_specification.py
@@ -10,11 +10,12 @@ with open("templates/specification.template", "r") as f:
     specification_template = f.read()
 
 # Load the global and variable attribute tables from the schema
-SCHEMA_URL = "https://raw.githubusercontent.com/ACCESS-NRI/schema/refs/heads/main/au.org.access-nri/model/output/file-metadata/2-0-0/2-0-0.json"
-global_table, variable_table = schema2md(SCHEMA_URL)
+SCHEMA_URL = "https://raw.githubusercontent.com/ACCESS-NRI/schema/refs/heads/main/au.org.access-nri/model/output/file-metadata/2-1-0/2-1-0.json"
+global_table, variable_table, time_md_str = schema2md(SCHEMA_URL)
 d = {
     "global_attrs_table": global_table,
     "variable_attrs_table": variable_table,
+    "time_attrs_table": time_md_str,
 }
 
 # Put the tables into the template and output to file
